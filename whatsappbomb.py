cd3883d9083d41c9a4b42404802438fbce6a6876
sudnyeshtalekar <48767586+sudnyeshtalekar@users.noreply.github.com>
2020-01-12 23:54:12 +0530

    Add files via upload

--- whatsappbomb.py
+++ whatsappbomb.py
@@ -1,22 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+
 
 driver = webdriver.Firefox()
-driver.get('http://web.whatsapp.com')
 
-name = input('Enter the name of user or group : ')
-msg = input('Enter the message : ')
-count = int(input('Enter the count : '))
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
 
-#Scan the code before proceeding further
-input('Enter anything after scanning QR code')
+# Replace 'Friend's Name' with the name of your friend 
+# or the name of a group 
+target = '"SRP VIT Pune"'
 
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
+# Replace the below string with your own message
+string = 'You are hacked!!!!'
+count=int(10)
 
-msg_box = driver.find_element_by_class_name('_13mgZ')
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+group_title.click()
 
 for i in range(count):
-    msg_box.send_keys(msg)
-    button=driver.find_element_by_class_name('hnQHL')
-    button.click()
+	message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+	message.send_keys(string)
 
+	sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+	sendbutton.click()
+
+driver.close()
